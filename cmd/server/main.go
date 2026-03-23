4d894a0f89b0ff714da542561185d1dd20176236
mattnickolaus <mattnickolaus@gmail.com>
2026-03-23 19:56:32 -0400

    setting up RabbitMQ connection server-side

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril server...")
+
+	connStr := "amqp://guest:guest@localhost:5672/"
+	rmq, err := amqp.Dial(connStr)
+	if err != nil {
+		log.Fatalf("Failed to connect to amqp with given connection stirng: %s", connStr)
+	}
+	defer rmq.Close()
+	println("Connection successful!")
+
+	// Create a channel to read the os.Signal to wait for Ctrl+C interrupt
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+
+	s := <-signalChan
+	fmt.Printf("\nReceived signal: %s\n", s)
+	fmt.Printf("Program shutting down...\n")
+	os.Exit(0)
 }
