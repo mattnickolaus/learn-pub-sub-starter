41315df7d121d770c5df72687f6f3de884ef2b52
mattnickolaus <mattnickolaus@gmail.com>
2026-03-31 16:36:55 -0400

    setting up a transient client queue in rabbitmq

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,7 +1,53 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+	"github.com/mattnickolaus/learn-pub-sub-starter/internal/gamelogic"
+	"github.com/mattnickolaus/learn-pub-sub-starter/internal/pubsub"
+	"github.com/mattnickolaus/learn-pub-sub-starter/internal/routing"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril client...")
+
+	connStr := "amqp://guest:guest@localhost:5672/"
+	rmq, err := amqp.Dial(connStr)
+	if err != nil {
+		log.Fatalf("Failed to connect to amqp with given connection stirng: %s", connStr)
+	}
+	defer rmq.Close()
+	println("Connection successful!")
+
+	userName, err := gamelogic.ClientWelcome()
+	if err != nil {
+		log.Fatalf("%v", err)
+	}
+
+	_, _, err = pubsub.DeclareAndBind(rmq,
+		routing.ExchangePerilDirect,
+		routing.PauseKey+"."+userName,
+		routing.PauseKey,
+		pubsub.SimpleQueueType{
+			Transient: true,
+		},
+	)
+	if err != nil {
+		log.Fatalf("Error during declare and bind: %v", err)
+	}
+
+	// Create a channel to read the os.Signal to wait for Ctrl+C interrupt
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+
+	s := <-signalChan
+	fmt.Printf("\nReceived signal: %s\n", s)
+	fmt.Printf("Program shutting down...\n")
+	os.Exit(0)
+
 }
--- internal/pubsub/publish_json.go
+++ internal/pubsub/publish_json.go
@@ -30,3 +30,34 @@ func PublishJSON[T any](ch *amqp.Channel, exchange, key string, val T) error {
 
 	return nil
 }
+
+type SimpleQueueType struct {
+	Durable   bool
+	Transient bool
+}
+
+func DeclareAndBind(
+	conn *amqp.Connection,
+	exchange,
+	queueName,
+	key string,
+	queueType SimpleQueueType, // SimpleQueueType is an "enum" type I made to represent "durable" or "transient"
+) (*amqp.Channel, amqp.Queue, error) {
+
+	ch, err := conn.Channel()
+	if err != nil {
+		return nil, amqp.Queue{}, err
+	}
+
+	q, err := ch.QueueDeclare(queueName, queueType.Durable, queueType.Transient, queueType.Transient, false, nil)
+	if err != nil {
+		return nil, amqp.Queue{}, err
+	}
+
+	err = ch.QueueBind(queueName, key, exchange, false, nil)
+	if err != nil {
+		return nil, amqp.Queue{}, err
+	}
+
+	return ch, q, nil
+}
